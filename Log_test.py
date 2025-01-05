b8ebd3989ce3f30a5ba54f5c75310e229c04c3b8
Sajjad17ab <Sheikhmohdsajjad1997@gmail.com>
2025-01-05 19:48:44 +0530

    Update Log_test.py

--- Log_test.py
+++ Log_test.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import tableauserverclient as TSC
-import logging
 
 # Streamlit UI
 st.title("Tableau View Exporter - Login & Upload")
@@ -11,14 +10,8 @@ site = st.text_input("Site name")
 token_name = st.text_input("Personal Access Token name")
 token_value = st.text_input("Personal Access Token value", type="password")
 
-# Logging level (optional)
-logging_level = st.selectbox("Logging level", ["debug", "info", "error"])
-
 # Action button to trigger the login test
 if st.button("Test Login"):
-    # Set up logging
-    logging.basicConfig(level=getattr(logging, logging_level.upper()))
-
     # Tableau Authentication
     tableau_auth = TSC.PersonalAccessTokenAuth(token_name, token_value, site_id=site)
     server_connection = TSC.Server(server, use_server_version=True, http_options={"verify": False})
