5cae950e7479a4f76725ac61ebdd1b6f42850ff4
Sajjad17ab <Sheikhmohdsajjad1997@gmail.com>
2025-01-05 19:51:36 +0530

    Update Log_test.py

--- Log_test.py
+++ Log_test.py
@@ -2,7 +2,7 @@ import streamlit as st
 import tableauserverclient as TSC
 
 # Streamlit UI
-st.title("Tableau View Exporter - Login & Upload")
+st.title("Tableau Login Test")
 
 # Input fields for server details
 server = st.text_input("Server address")
@@ -20,19 +20,6 @@ if st.button("Test Login"):
         # Attempt to sign in
         with server_connection.auth.sign_in(tableau_auth):
             st.write("Connected to Tableau Server successfully!")
-
-            # Get the server information (ServerInfoItem)
-            server_info = server_connection.server_info
-
-            # Access attributes from the server_info object
-            product_version = server_info.product_version
-            build_number = server_info.build_number
-            rest_api_version = server_info.rest_api_version
-
-            # Displaying the information
-            st.write(f"Tableau Product Version: {product_version}")
-            st.write(f"Tableau Build Number: {build_number}")
-            st.write(f"REST API Version: {rest_api_version}")
 
     except TSC.ServerResponseError as e:
         st.error(f"Server response error: {str(e)}")
