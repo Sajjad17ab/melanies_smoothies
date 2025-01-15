a82bd229a96f32307503500b530721ff01fbc704
Sajjad17ab <Sheikhmohdsajjad1997@gmail.com>
2025-01-15 20:34:51 +0530

    Update Tableau_app.py

--- Tableau_app.py
+++ Tableau_app.py
@@ -20,7 +20,7 @@ site_id = st.text_input("Enter your Tableau Site ID (Leave blank for default sit
 server_url = st.text_input("Enter Tableau Server URL", value="https://prod-apnortheast-a.online.tableau.com")
 
 # Dropdown to switch between create project, content info, publish workbook, create group, and create schedules
-option = st.selectbox("Select an option:", ["Content Info", "Create Project", "Publish Workbook", "Create Group", "Create Schedules"])
+option = st.selectbox("Select an option:", ["Content Info", "Create Project", "Publish Workbook", "Create Group"])
 
 # If the user selects "Content Info"
 if option == "Content Info":
@@ -208,67 +208,3 @@ elif option == "Create Group":
                 st.error(f"An error occurred while creating the group: {e}")
         else:
             st.error("Please provide a group name.")
-
-# If the user selects "Create Schedules"
-elif option == "Create Schedules":
-    if st.button("Create Schedules"):
-        if token_name and token_value and server_url:
-            try:
-                tableau_auth = TSC.PersonalAccessTokenAuth(token_name, token_value, site_id=site_id)
-                server = TSC.Server(server_url, use_server_version=False)
-                server.add_http_options({"verify": False})
-                server.use_server_version()
-
-                with server.auth.sign_in(tableau_auth):
-                    # Hourly Schedule
-                    hourly_interval = TSC.HourlyInterval(start_time=time(2, 30), end_time=time(23, 0), interval_value=2)
-                    hourly_schedule = TSC.ScheduleItem(
-                        "Hourly-Schedule",
-                        50,
-                        TSC.ScheduleItem.Type.Extract,
-                        TSC.ScheduleItem.ExecutionOrder.Parallel,
-                        hourly_interval,
-                    )
-                    hourly_schedule = server.schedules.create(hourly_schedule)
-                    st.write(f"Hourly schedule created (ID: {hourly_schedule.id}).")
-
-                    # Daily Schedule
-                    daily_interval = TSC.DailyInterval(start_time=time(5))
-                    daily_schedule = TSC.ScheduleItem(
-                        "Daily-Schedule",
-                        60,
-                        TSC.ScheduleItem.Type.Subscription,
-                        TSC.ScheduleItem.ExecutionOrder.Serial,
-                        daily_interval,
-                    )
-                    daily_schedule = server.schedules.create(daily_schedule)
-                    st.write(f"Daily schedule created (ID: {daily_schedule.id}).")
-
-                    # Weekly Schedule
-                    weekly_interval = TSC.WeeklyInterval(time(19, 15), TSC.IntervalItem.Day.Monday, TSC.IntervalItem.Day.Wednesday, TSC.IntervalItem.Day.Friday)
-                    weekly_schedule = TSC.ScheduleItem(
-                        "Weekly-Schedule",
-                        70,
-                        TSC.ScheduleItem.Type.Extract,
-                        TSC.ScheduleItem.ExecutionOrder.Serial,
-                        weekly_interval,
-                    )
-                    weekly_schedule = server.schedules.create(weekly_schedule)
-                    st.write(f"Weekly schedule created (ID: {weekly_schedule.id}).")
-
-                    # Monthly Schedule
-                    monthly_interval = TSC.MonthlyInterval(start_time=time(23, 30), interval_value=15)
-                    monthly_schedule = TSC.ScheduleItem(
-                        "Monthly-Schedule",
-                        80,
-                        TSC.ScheduleItem.Type.Subscription,
-                        TSC.ScheduleItem.ExecutionOrder.Parallel,
-                        monthly_interval,
-                    )
-                    monthly_schedule = server.schedules.create(monthly_schedule)
-                    st.write(f"Monthly schedule created (ID: {monthly_schedule.id}).")
-
-            except Exception as e:
-                st.error(f"An error occurred while creating the schedules: {e}")
-        else:
-            st.error("Please provide all the necessary credentials.")
